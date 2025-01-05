ac25190a4f2c726fc650354aa445f8ad8a29b11a
Lawzen <davidlumu2@gmail.com>
2025-01-05 18:29:14 +0100

    Fix Spring

--- SpringBoot/anticair/src/main/java/com/anticair/app/repository/AnnonceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anticair.app.repository;
-
-import com.anticair.app.entity.Annonce;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AnnonceRepository extends JpaRepository<Annonce, Long> {
-}
